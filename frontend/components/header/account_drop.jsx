b5e7ed03bb893ec70a6a52ffd01330967d5e33d8
John Goddard <johngoddard243@gmail.com>
2016-09-08 21:45:27 -0700

    drop style

--- frontend/components/header/account_drop.jsx
+++ frontend/components/header/account_drop.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router';
 
 export default ({exitSidebar, handleLogout}) => (
   <ul className='header-drop'>
+    <Link to={'/app/account'}>
     <li className='account-link' onClick={exitSidebar}>
-      <Link to={'/app/account'}>
-        <span className='glyphicon glyphicon-cog'></span>
-        Account
-      </Link>
+      <span className='glyphicon glyphicon-cog'></span>
+      Account
     </li>
+    </Link>
     <li className='sign-out-link' onClick={handleLogout}>
       <span className='glyphicon glyphicon-log-out'></span>
       Sign Out
